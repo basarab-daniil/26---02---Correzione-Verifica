b064b6c1083aa0db13710bfa2935340700068426
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2025-02-22 11:22:38 +0000

    punto A

---
+++ script.js
@@ -0,0 +1,21 @@
+const canzone1 = {
+    titolo: "Il secondo secondo me",
+    immagine_disco: "images/veritasupposte.jpg",
+    informazioni: "prende di mira una serie di luoghi comuni; inoltre tratta dei pregiudizi che si hanno contro altre persone.",
+    audio_canzone: "audio/alone.mp3",
+    link: "https://youtu.be/10DIJs67ReI?feature=shared"
+};
+const canzone2 = {
+    titolo: "Ricchi e morti",
+    immagine_disco: "images/playlist.jpg",
+    informazioni: "Seppur nel testo non ci siano riferimenti espliciti, il titolo allude probabilmente alla serie animata statunitense Rick and Morty.",
+    audio_canzone: "audio/spinninghead.mp3",
+    link: "https://youtu.be/jMC1qSotmv4?feature=shared"
+};
+const canzone3 = {
+    titolo: "Lucifero",
+    immagine_disco: "images/nomidiavolo.jpg",
+    informazioni: "In “Lucifero”, traccia finale dell'album, Yugi si cimenta in un monologo introspettivo, quasi un flusso di coscienza, in cui affronta vari argomenti, tra cui il fine della sua musica.",
+    audio_canzone: "audio/vlogmusic.mp3",
+    link: "https://youtu.be/3F0LppqS6AU?feature=shared"
+};
